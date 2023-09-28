47e298d9bd9cce344cb7d551ed522304e8e7f516
leoguimaraes49 <leoguimasvm@gmail.com>
2023-09-28 20:00:21 -0300

    Adicionando ClasseA e ClasseB

---
+++ ClasseA.java
@@ -0,0 +1,28 @@
+public class ClasseA {
+    private int a1;
+    private double a2;
+
+    public int getA1() {
+        return a1;
+    }
+
+    public void setA1(int a1) {
+        this.a1 = a1;
+    }
+    
+    public double getA2() {
+        return a2;
+    }
+
+    public void setA2(double a2) {
+        this.a2 = a2;
+    }
+
+    public void MA1 () {
+        System.out.println("MA1");
+    }
+
+    public void MA2 () {
+        System.out.println("MA2");
+    }
+}
---
+++ ClasseB.java
@@ -0,0 +1,28 @@
+public class ClasseB {
+    private int b1;
+    private double b2;
+
+    public int getB1() {
+        return b1;
+    }
+
+    public void setB1(int b1) {
+        this.b1 = b1;
+    }
+
+    public double getB2() {
+        return b2;
+    }
+
+    public void setB2(double b2) {
+        this.b2 = b2;
+    }
+
+    public void MB1 () {
+        System.out.println("MB1");
+    }
+
+    public void MB2 () {
+        System.out.println("MB2");
+    }
+}
