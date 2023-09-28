5ffc0ee248a1219b0641fb0e4222c648bd52eeef
leoguimaraes49 <leoguimasvm@gmail.com>
2023-09-28 20:23:27 -0300

    Criacao Classe C

---
+++ ClasseC.java
@@ -0,0 +1,29 @@
+public class ClasseC {
+    private int c1;
+    private double c2;
+
+    public int getC1() {
+        return c1;
+    }
+
+    public void setC1(int c1) {
+        this.c1 = c1;
+    }
+
+    public double getC2() {
+        return c2;
+    }
+
+    public void setC2(double c2) {
+        this.c2 = c2;
+    }
+
+    public void MC1 () {
+        System.out.println("MC1");
+    }
+
+    public void MC2 () {
+        System.out.println("MC2");
+    }
+}
+
