3ba43784e273efc652b8fe025bd952a6f8409551
leoguimaraes49 <leoguimasvm@gmail.com>
2023-10-03 01:15:42 -0300

    Criação da Classe D

---
+++ ClasseD.java
@@ -0,0 +1,28 @@
+public class ClasseD {
+    private int D1;
+    private double D2;
+
+    public int getD1() {
+        return D1;
+    }
+
+    public void setD1(int d1) {
+        D1 = d1;
+    }
+
+    public double getD2() {
+        return D2;
+    }
+
+    public void setD2(double d2) {
+        D2 = d2;
+    }
+
+    public void MD1() {
+        System.out.println("MD1");
+    }
+
+    public void MD2() {
+        System.out.println("MD2");
+    }
+}
